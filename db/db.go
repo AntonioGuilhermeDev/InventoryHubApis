810a8177d433c0ed354e5d3f9e2dc6bfbfa3864f
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-05-24 20:19:15 -0300

    Criando tabelas de usuarios, estabelecimentos e enderecos

--- db/db.go
+++ db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/lib/pq"
 )
@@ -37,4 +38,72 @@ func InitDB() {
 
 	fmt.Println("Conexão realizada com sucesso!")
 
+	err = createTables()
+	if err != nil {
+		panic(fmt.Sprintf("Erro ao criar tabelas: %v", err))
+	}
+}
+
+func createTables() error {
+	createEnderecosTable := `
+	CREATE TABLE IF NOT EXISTS enderecos (
+		id SERIAL PRIMARY KEY,
+		logradouro VARCHAR(100) NOT NULL,
+		complemento VARCHAR(50),
+		numero INTEGER NOT NULL,
+		bairro VARCHAR(50) NOT NULL,
+		cidade VARCHAR(50) NOT NULL,
+		uf CHAR(2) NOT NULL,
+		cep VARCHAR(9) NOT NULL
+	);
+	`
+	_, err := DB.Exec(createEnderecosTable)
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("Tabela 'enderecos' criada com sucesso.")
+
+	createEcsTable := `
+	CREATE TABLE IF NOT EXISTS estabelecimentos (
+		id SERIAL PRIMARY KEY,
+		razao_social VARCHAR(255) NOT NULL,
+		created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+		updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+		endereco_id INTEGER NOT NULL,
+		FOREIGN KEY (endereco_id) REFERENCES enderecos(id) ON DELETE CASCADE
+	);
+	`
+	_, err = DB.Exec(createEcsTable)
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("Tabela 'estabelecimentos' criada com sucesso.")
+
+	createUsersTable := `
+	CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		email TEXT NOT NULL UNIQUE,
+		nome VARCHAR(50) NOT NULL,
+		sobrenome VARCHAR(50) NOT NULL,
+		password TEXT NOT NULL,
+		created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+		updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+		role VARCHAR(20) NOT NULL CHECK (role IN ('OWNER', 'MANAGER', 'SELLER')),
+		estabelecimento_id INTEGER NOT NULL,
+		FOREIGN KEY (estabelecimento_id) REFERENCES estabelecimentos(id) ON DELETE CASCADE
+	);
+	`
+	_, err = DB.Exec(createUsersTable)
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("Tabela 'users' criada com sucesso.")
+
+	return nil
 }
