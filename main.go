803867c6827a1f467b39173dcab60810eff1b0bb
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-07-26 17:18:29 -0300

    Atualizando dependências e configurando CORS

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"time"
+
 	"github.com/AntonioGuilhermeDev/InventoryHubApis/db"
 	"github.com/AntonioGuilhermeDev/InventoryHubApis/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -16,6 +19,15 @@ func main() {
 
 	db.InitDB()
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	routes.RegisterRoutes(server)
 
